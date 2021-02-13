404b5c77c1a4e71225c56adb98273c05cef66af1
Rudzinski <kostek660i@gmail.com>
2021-02-13 14:38:31 +0100

    Obtain a sha from bitbucket by Guzzlehttp

--- src/Controller/controller.php
+++ src/Controller/controller.php
@@ -1,18 +1,33 @@
 <?php
-
 namespace KubeEnd\Controller;
+require "../../vendor/autoload.php";
+
 
 use GuzzleHttp\Client;
 
-$client = new Client();
-$request = $client->request('GET', 'https://api.github.com/repos/'.$nick.'/'.$repo.'/commits');
-$request1 = $client->request('GET', 'https://api.bitbucket.org/2.0/repositories/'.$nick.'/'.$repo.'/commits');
+//$nick = "kuba-end";
+//$repo = "kuba-end";
+$workspace = "kuba-end";
+$repoSlug = "last-git-commit";
 
+$client = new Client();
+/*$request = $client->request('GET', 'https://api.github.com/repos/'.$nick.'/'.$repo.'/commits');
 $response =$request->getBody();
 $response_as_array = ((json_decode($response,true)));
 $last_commit_sha = ($response_as_array[0]["sha"]);
-$last_commit_url = ($response_as_array[0]["html_url"]);
+$last_commit_url = ($response_as_array[0]["html_url"]);*/
 
-class ChoosePlatform{
-    return "1";
-}
+$request1 = $client->request('GET', 'https://api.bitbucket.org/2.0/repositories/'.$workspace.'/'.$repoSlug.'/commits');
+$response1 =$request1->getBody();
+//var_dump($response1);
+$response_as_array1 = ((json_decode($response1,true)));
+var_dump($response_as_array1);
+$last_commit_sha1 = ($response_as_array1["values"][0]["hash"]);
+//$last_commit_url1 = ($response_as_array1[0]["html_url"]);
+
+//echo $last_commit_sha.PHP_EOL;
+//echo $last_commit_url.PHP_EOL;
+echo $last_commit_sha1.PHP_EOL;
+//echo $last_commit_url1.PHP_EOL;
+
+
