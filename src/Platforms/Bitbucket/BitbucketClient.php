cfde563cef445a899206fc2dd19015c8a990c0ac
Rudzinski <kostek660i@gmail.com>
2021-03-03 20:04:34 +0100

    Some improvments in clients

--- src/Platforms/Bitbucket/BitbucketClient.php
+++ src/Platforms/Bitbucket/BitbucketClient.php
@@ -4,36 +4,29 @@
 namespace KubaEnd\Platforms\Bitbucket;
 
 
-use KubaEnd\Platforms\Common\AbstractPlatform;
+use KubaEnd\Platforms\Common\AbstractClient;
 use KubaEnd\Platforms\Interfaces\ClientInterface;
-use Psr\Http\Message\ResponseInterface;
-use RuntimeException;
 
-class BitbucketClient extends AbstractPlatform implements ClientInterface
+
+class BitbucketClient extends AbstractClient implements ClientInterface
 {
-    /**
-     * @var string
-     */
-    protected string $baseUrl;
 
-    public function decode(ResponseInterface $response): array
-    {
-        $body=json_decode($response->getBody(),true);
-        if (json_last_error()!==JSON_ERROR_NONE){
-            throw new RuntimeException(
-                sprintf('Cannot decode response.')
-            );
-        }
-        return $body;
-    }
+    const BITBUCKET_BASE_URI="https://api.bitbucket.org/2.0";
+
+    /**
+     * Http client request returning decoded response from bitbucket
+     * @param string $username
+     * @param string $repositoryName
+     * @return array
+     * @throws
+     */
     public function request(string $username, string $repositoryName): array
     {
         $method='GET';
-        $this->baseUrl="https://api.bitbucket.org/2.0";
         $username='/repositories/'.$username.'/';
         $repositoryName=$repositoryName.'/commits';
-        $uri=$this->baseUrl.$username.$repositoryName;
-        return $this->client->request($method,$uri);
+        $uri=self::BITBUCKET_BASE_URI.$username.$repositoryName;
+        return $this->decode($this->client->request($method,$uri));
 
     }
 }
---
+++ src/Platforms/Common/AbstractClient.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace KubaEnd\Platforms\Common;
+
+
+use GuzzleHttp\Client as HttpClient;
+use KubaEnd\Platforms\Interfaces\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
+
+abstract class AbstractClient implements ClientInterface
+{
+    protected $client;
+
+
+    public function __construct(){
+        $this->client= new HttpClient([
+            'verify' => false
+        ]);
+    }
+    public function decode(ResponseInterface $response): array
+    {
+        $body = json_decode($response->getBody(), true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \RuntimeException(
+                sprintf('Cannot decode response.')
+            );
+        }
+
+        return $body;
+    }
+}
